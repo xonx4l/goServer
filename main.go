2ddadbecc086a833ac982b7d77c573d4d25da7d3
xonx4l <sk797119@gmail.com>
2023-09-28 16:57:29 +0530

    okay

--- main.go
+++ main.go
@@ -2,15 +2,30 @@ package main
 
 import "fmt"
        "os"
+	   "Log"
 
 func main() {
 	fmt.Println("hello world")
+
+	godotenv.Load(".env")
 
 	portString := os.Getenv("PORT")
 	if portString = "" {
 		log.fatal("PORT is not found in the environment")
 
 	}
+
+	router := chi.NewRouter()
+
+	srv:= &http.server{
+		Handler:router,
+		Addr: ":" + portString,
+	}
+
+     err = srv.listenAndServer()
+	 if err != nil{
+		log.fatal(err)
+	 }
 
 	fmt.println("Port:", portString)
 }
