32b1ff4e3a528d7f8ef8d80f42ff3a32ec07f6f4
xonx4l <sk797119@gmail.com>
2023-10-14 16:30:12 +0530

    yo

--- handler_user.go
+++ handler_user.go
@@ -1,7 +1,21 @@
 package main
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.request) {
+	type parameters struct {
+		Name string `name`
+	}
+	decoder := json.NewDecoder(r.Body)
+
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, 400, fmt.sprintf("Error parsing JSON:", err))
+	}
+
 	respondWithJSON(w, 200, struct{}{})
 }
